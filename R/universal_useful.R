3256d7f6e97dd1bfb0ef2ea414e9c10764f7f190
Stephen Holsenbeck <sholsen@alumni.emory.edu>
2022-01-05 08:56:08 -0500

    `file_fn`: differentiate read/write for jpg/png extensions

--- R/universal_useful.R
+++ R/universal_useful.R
@@ -121,10 +121,12 @@ file_fn <- function(x, write = FALSE) {
     grepl("feather$", ., ignore.case = TRUE) && !write ~ feather::read_feather,
     grepl("rds$", ., ignore.case = TRUE) && !write ~ readRDS,
     grepl("(?:rda$)|(?:rdata$)", ., ignore.case = TRUE) && !write ~ load_obj,
+    grepl("(?:png$)|(?:jpg$)|(?:jpeg$)", ., ignore.case = TRUE) && !write ~ purrr::when(UU::is_legit(utils::packageVersion("magick")), . ~ magick::image_read,
     grepl("csv$", ., ignore.case = TRUE) ~ readr::write_csv,
     grepl("feather$", ., ignore.case = TRUE) ~ feather::write_feather,
     grepl("rds$", ., ignore.case = TRUE) ~ saveRDS,
-    grepl("(?:png$)|(?:jpg$)|(?:jpeg$)", ., ignore.case = TRUE) ~ purrr::when(UU::is_legit(utils::packageVersion("magick")), . ~ magick::image_read, ~ stop(x, " is an image and requires the magick package.")
+    grepl("(?:png$)|(?:jpg$)|(?:jpeg$)", ., ignore.case = TRUE) ~ ggplot2::ggsave,
+     ~ stop(x, " is an image and requires the magick package.")
   ),
   grepl("(?:rda$)|(?:rdata$)", ., ignore.case = TRUE) ~ save
   )
