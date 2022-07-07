952d4b516aeeee80c7c7ad8f3b3c3a07f1c94ec1
Stephen Holsenbeck <stephen@virgalabs.io>
2022-07-06 20:19:06 -0400

    Update load order of UU::startup: user first, project second

--- R/universal_useful.R
+++ R/universal_useful.R
@@ -275,10 +275,12 @@ is_project <- function() {
 startup <- function() {
   if (!getOption("UU_startup", FALSE)) {
     options(UU_startup = TRUE)
-    list(.Rprofile = Sys.getenv("R_PROFILE" , ".Rprofile"),
-         .Rprofile_user = Sys.getenv("R_PROFILE_USER", "~/.Rprofile"),
-         .Renviron = Sys.getenv("R_ENVIRON", ".Renviron"),
-         .Renviron_user = Sys.getenv("R_ENVIRON_USER", "~/.Renviron")) |>
+    list(
+      .Rprofile_user = Sys.getenv("R_PROFILE_USER", "~/.Rprofile"),
+      .Rprofile = Sys.getenv("R_PROFILE" , ".Rprofile"),
+      .Renviron_user = Sys.getenv("R_ENVIRON_USER", "~/.Renviron"),
+      .Renviron = Sys.getenv("R_ENVIRON", ".Renviron")
+    ) |>
       purrr::iwalk(~{
         if (file.exists(.x)) {
           gmsg("{.path {.x}} loaded.")
