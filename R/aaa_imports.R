8e8618834f6ff9e7e1e4342a0593cdcd00eff2a8
Stephen Holsenbeck <stephen@virgalabs.io>
2022-10-05 21:00:54 -0400

    export plyr::round_any

--- R/aaa_imports.R
+++ R/aaa_imports.R
@@ -1,6 +1,8 @@
-#' @importFrom rlang `%|%` `%||%`
+#' @importFrom rlang `%|%` `%||%` is_error
+#' @importFrom plyr round_any
 #' @export
 rlang::`%||%`
 #' @export
 rlang::`%|%`
-
+#' @export
+plyr::round_any
