95f5c1f69f5d314b4c51448885bdaa2422ea8cff
Stephen Holsenbeck <stephen@virgalabs.io>
2023-01-13 18:34:18 -0500

    Add `luminance_filter`

--- R/css_color_helpers.R
+++ R/css_color_helpers.R
@@ -96,6 +96,52 @@ color_luminance <- function(red, green, blue, alpha, ...) {
 }
 
 
+#' Filter colors based on a luminance threshold
+#'
+#' @param colors \code{chr} color vector
+#' @param dark_mode \code{lgl} colors will be `>=` if `TRUE`, otherwise `<=` if `FALSE`
+#' @param luminance_threshold \code{lgl} 0-1 luminance threshold to filter
+#'
+#' @return \code{chr} vector of colors
+#' @export
+#'
+#' @examples
+#' x <- tibble::tribble(
+#' ~CSS.Name, ~Hex.Code, ~Decimal.Code,
+#' "Black",        "#000000",            "(0,0,0)",
+#' "White",        "#FFFFFF",      "(255,255,255)",
+#' "Red",        "#FF0000",          "(255,0,0)",
+#' "Lime",        "#00FF00",          "(0,255,0)",
+#' "Blue",        "#0000FF",          "(0,0,255)",
+#' "Yellow",        "#FFFF00",        "(255,255,0)",
+#' "Cyan / Aqua",        "#00FFFF",        "(0,255,255)",
+#' "Magenta / Fuchsia",        "#FF00FF",        "(255,0,255)",
+#' "Silver",        "#C0C0C0",      "(192,192,192)",
+#' "Gray",        "#808080",      "(128,128,128)",
+#' "Maroon",        "#800000",          "(128,0,0)",
+#' "Olive",        "#808000",        "(128,128,0)",
+#' "Green",        "#008000",          "(0,128,0)",
+#' "Purple",        "#800080",        "(128,0,128)",
+#' "Teal",        "#008080",        "(0,128,128)",
+#' "Navy",        "#000080",          "(0,0,128)"
+#' )
+#' luminance_filter(x$Hex.Code, TRUE)
+#' luminance_filter(x$Hex.Code, FALSE)
+luminance_filter <- function(colors, dark_mode, luminance_threshold = .5) {
+  luminance <- UU::color_rgb_table(colors) |>
+    purrr::pmap_dbl(\(...) {
+      .x <- list(...)
+      rlang::exec(UU::color_luminance, !!!.x)
+    })
+  op <- if (dark_mode)
+    base::`>=`
+  else
+    base::`<=`
+
+  i <- op(luminance, luminance_threshold)
+  colors[i]
+}
+
 #' Compute color distance
 #'
 #' @param x \code{num/chr} A CSS hex or rgb/rgba color, or a numeric vector of r,g,b values
