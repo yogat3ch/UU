73cebfae856b8c5048d7b3b61e92292f86a47862
Stephen Holsenbeck <stephen@virgalabs.io>
2023-04-26 10:04:24 -0400

    escape the $

--- R/opts_helpers.R
+++ R/opts_helpers.R
@@ -4,7 +4,7 @@
 #'
 #' @seealso write_opts
 #' Code is dynamically generated when package is loaded. These will not auto generate properly in a deployed shiny app. See \link{write_opts} for a non-interactive alternative.
-#' @usage opts$your_option_name()
+#' @usage opts\$your_option_name()
 #' @param default if the specified option is not set in the options list, this value is returned. This facilitates retrieving an option and checking whether it is set and setting it separately if not.
 #' @return \code{lgl}
 #' @family options
@@ -81,8 +81,8 @@ write_opts <- function(file = "R/utils_opts.R") {
 
 #' Toggle or change an option listed in a local _.Rprofile_ for the session
 #' @description Any options in the project local _.Rprofile_ will populate this object as named methods. These named methods, when called, will toggle the options on or off. Alternatively, if an option is not logical, it can be changed using the `set` argument. This loads at the start of an R session if \link[UU]{startup} has been called in the user-level _.Rprofile_. These methods are meant for interactive use only.
-#' @usage toggle$your_logical_option_name()
-#' @usage toggle$your_non_logical_option_name(set = "value")
+#' @usage toggle\$your_logical_option_name()
+#' @usage toggle\$your_non_logical_option_name(set = "value")
 #' @param opt \code{chr} option name to toggle. Dynamically populated
 #' @param set \code{obj} value to set the option to (takes precedence over toggle)
 #' @family options
