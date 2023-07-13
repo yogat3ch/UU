22a82cbbaad1d3f1a2a1c7c88d385dcfbc51fd87
Stephen Holsenbeck <stephen@virgalabs.io>
2023-07-13 11:58:18 -0400

    Docs

--- R/universal_useful.R
+++ R/universal_useful.R
@@ -735,7 +735,7 @@ fn_name <- function(fn) {
 #' Print function formals as a list
 #'
 #' @param f \code{fun}
-#'
+#' @param to_console \code{lgl} whether to print \code{\link[base]{dput}} output to console. ** Default: FALSE **
 #' @return \code{msg}
 #' @export
 #'
