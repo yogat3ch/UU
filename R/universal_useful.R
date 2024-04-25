7d300ac17586f7ce86329597d538e08d36cb7cd7
Stephen Holsenbeck <sholsen@alumni.emory.edu>
2024-04-25 16:12:10 -0400

    `formula_make` to universal useful

--- R/universal_useful.R
+++ R/universal_useful.R
@@ -1185,3 +1185,18 @@ filter_to <- function(filters, out_type = character(), .data) {
     out <- filters
   return(out)
 }
+
+#' Create a formula given predictors and a label (response variable)
+#'
+#' @param predictors \code{chr} Column names used for prediction
+#' @param label \code{chr} Column name of response variable
+#'
+#' @return \code{formula}
+#' @export
+#'
+#' @examples
+#' formula_make(predictors = c("max2yrlfflow", "min2yrlfflow"))
+formula_make <- function(predictors, label = "response") {
+  formula(paste0(label," ~ ", paste(collapse = " + ", glue::glue("`{predictors}`"))))
+}
+
